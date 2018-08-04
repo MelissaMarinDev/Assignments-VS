b0273d75f742d772b7d60d08ea4e8a60ffa728e4
MelissaMarinDev <40775567+MelissaMarinDev@users.noreply.github.com>
2018-08-04 15:43:39 -0600

    Update ColAdvFunctions.js
    8.4.2018 3:45pm

--- projects/ColossalAdventure-consoleRPG/rpg-prep-notes/ColAdvFunctions.js
+++ projects/ColossalAdventure-consoleRPG/rpg-prep-notes/ColAdvFunctions.js
@@ -1,25 +1,48 @@
 
 function walk(){
-    var chance
-}
-function isFight(){
+    chance();
+    if (chance > 3){
+       console.log('Walking is good for the soul and for you HP. Your HP just increased 10 points.');
+        userHP += 10;
+        return userHP;
+    } else {
+        whatEnemy() //will return the enemy or the type of enemy
+        enemyAppears() 
+    }
     
 }
-function inventory(){
+function enemyAppears(){
+    while(userHP > 0 || enemyHP > 0) {
+        fightMenu = ['Run', 'Fight'];
 
+        index = ask.keyInSelect(fightMenu, 'What would you like to do? ');   
 }
-function hp(){
-
+function whatEnemy(){
+    var temp = Math.floor(Math.random() * 3); // will have options 0 to 2
+    if (temp === 0 ){
+        console.log('It is a dwarf!');
+        enemy = 'dwarf';
+        enemyHP = 20;
+        return enemy enemyHP;
+    }
+    if (temp === 1 ){
+        console.log('It is an evil knight!');
+        enemy = 'evil knight';
+        enemyHP = 40;
+        return enemy enemyHP;
+    }
+    if (temp === 2 ){
+        console.log('It is an orge!');
+        enemy = 'orge';
+        enemyHP = 60;
+        return enemy enemyHP;
+    }
+    
 }
-function fight(){
-
+fuction chance(){
+    chance = Math.floor(Math.random() * 11);
+    return chance;
 }
-function run(){
-
-}
-
-
-walk, inventory, hp, fight, run
 
 
 module.exports = { 
